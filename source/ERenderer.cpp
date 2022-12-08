befc358267722152a86b1916ba1cec235783dde7
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2022-12-08 21:00:39 +0100

    DirectX Initialized

--- source/ERenderer.cpp
+++ source/ERenderer.cpp
@@ -15,9 +15,9 @@ Elite::Renderer::Renderer(SDL_Window * pWindow)
 	m_Height = static_cast<uint32_t>(height);
 
 	//Initialize DirectX pipeline
-	//...
-	//m_IsInitialized = true;
-	//std::cout << "DirectX is ready\n";
+	InitializeDirectX();
+	m_IsInitialized = true;
+	std::cout << "DirectX is ready\n";
 }
 
 Elite::Renderer::~Renderer()
@@ -29,4 +29,146 @@ void Elite::Renderer::Render()
 		return;
 }
 
+HRESULT Elite::Renderer::InitializeDirectX()
+{
+	// Create Device and  Device context, using hardware acceleration
+	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+	uint32_t createDeviceFlags = 0;
+#ifdef DEBUG || defined(_DEBUG)
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif	
+	HRESULT result = D3D11CreateDevice(0, D3D_DRIVER_TYPE_HARDWARE, 0, createDeviceFlags,
+		0, 0, D3D11_SDK_VERSION, &m_pDevice,
+		&featureLevel, &m_pDeviceContext);
+	if (FAILED(result))
+		return result;
 
+	//Create DXGI Factory to create SwapChain based on hardware
+	result = CreateDXGIFactory(__uuidof(IDXGIFactory), reinterpret_cast<void**>(&m_pDXGIFactory));
+	if (FAILED(result))
+		return result;
+
+	//Create SwapChain Descriptor
+	DXGI_SWAP_CHAIN_DESC swapChainDesc{};
+	swapChainDesc.BufferDesc.Width = m_Width;
+	swapChainDesc.BufferDesc.Height = m_Height;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 1;
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 60;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.Windowed = true;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Flags = 0;
+
+	//Get the handle HWND from the SDL backbuffer
+	SDL_SysWMinfo sysWMInfo{};
+	SDL_VERSION(&sysWMInfo.version);
+	SDL_GetWindowWMInfo(m_pWindow, &sysWMInfo);
+	swapChainDesc.OutputWindow = sysWMInfo.info.win.window;
+
+	//Create SwapChain and hook it into the handle of the SDL window
+	result = m_pDXGIFactory->CreateSwapChain(m_pDevice, &swapChainDesc, &m_pSwapChain);
+	if (FAILED(result))
+		return result;
+
+	//Create the Depth/Stencil Buffer and View
+	D3D11_TEXTURE2D_DESC depthStencilDesc{};
+	depthStencilDesc.Width = m_Width;
+	depthStencilDesc.Height = m_Height;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilDesc.SampleDesc.Count = 1;
+	depthStencilDesc.SampleDesc.Quality = 0;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+
+	//Create resource view view for Depth/Stencil Buffer
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc{};
+	depthStencilViewDesc.Format = depthStencilDesc.Format;
+	depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depthStencilViewDesc.Texture2D.MipSlice = 0;
+
+	//Create resource and matching resource view
+	result = m_pDevice->CreateTexture2D(&depthStencilDesc, 0, &m_pDepthStencilBuffer);
+	if (FAILED(result))
+		return result;
+
+	result = m_pDevice->CreateDepthStencilView(m_pDepthStencilBuffer, &depthStencilViewDesc, &m_pDepthStencilView);
+	if (FAILED(result))
+		return result;
+
+	//Create the RenderTargetView
+	result = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&m_pRenderTargetBuffer));
+	if (FAILED(result))
+		return result;
+	result = m_pDevice->CreateRenderTargetView(m_pRenderTargetBuffer, 0, &m_pRenderTargetView);
+	if (FAILED(result))
+		return result;
+
+	//Bind the Views to the Output Merger Stage
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, m_pDepthStencilView);
+
+	//Set the viewport
+	D3D11_VIEWPORT viewPort{};
+	viewPort.Width = static_cast<float>(m_Width);
+	viewPort.Height = static_cast<float>(m_Height);
+	viewPort.TopLeftX = 0.f;
+	viewPort.TopLeftY = 0.f;
+	viewPort.MinDepth = 1.f;
+	viewPort.MaxDepth = 1.f;
+	m_pDeviceContext->RSSetViewports(1, &viewPort);
+
+	//Clear Buffers
+	RGBColor clearColor = RGBColor(0.f, 0.f, 0.3f);
+	m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, &clearColor.r);
+	m_pDeviceContext->ClearDepthStencilView(m_pDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	//Render
+	// ...
+
+	//Present
+	m_pSwapChain->Present(0, 0);
+
+	//Release resources (to prevent resource leaks)
+	if (m_pRenderTargetView)
+	{
+		m_pRenderTargetView->Release();
+	}
+
+	if (m_pRenderTargetBuffer)
+	{
+		m_pRenderTargetBuffer->Release();
+	}
+
+	if (m_pDepthStencilView)
+		m_pDepthStencilView->Release();
+
+	if (m_pDepthStencilBuffer)
+		m_pDepthStencilBuffer->Release();
+
+	if (m_pSwapChain)
+		m_pSwapChain->Release();
+
+	if (m_pDeviceContext)
+	{
+		m_pDeviceContext->ClearState();
+		m_pDeviceContext->Flush();
+		m_pDeviceContext->Release();
+	}
+
+	if (m_pDevice)
+	{
+		m_pDevice->Release();
+	}
+
+	if (m_pDXGIFactory)
+		m_pDXGIFactory->Release();
+}
--- source/ERenderer.h
+++ source/ERenderer.h
@@ -33,6 +33,21 @@ namespace Elite
 		uint32_t m_Height;
 
 		bool m_IsInitialized;
+
+		ID3D11Device* m_pDevice;
+		ID3D11DeviceContext* m_pDeviceContext;
+
+		IDXGIFactory* m_pDXGIFactory;
+
+		IDXGISwapChain* m_pSwapChain;
+
+		ID3D11Resource* m_pRenderTargetBuffer;
+		ID3D11RenderTargetView* m_pRenderTargetView;
+
+		ID3D11Texture2D* m_pDepthStencilBuffer;
+		ID3D11DepthStencilView* m_pDepthStencilView;
+
+		HRESULT InitializeDirectX();
 	};
 }
 
